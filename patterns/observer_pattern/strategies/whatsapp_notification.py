7c825eaf7d0cbaafeb9c266a34dc12294fa8ac45
Satta Sundar Talukdar <bappu.ctg@gmail.com>
2025-02-04 02:15:14 +0600

    add: add email and whatsapp notification strategies

---
+++ patterns/observer_pattern/strategies/whatsapp_notification.py
@@ -0,0 +1,13 @@
+from patterns.observer_pattern.interface.notification_strategy import NotificationStrategy
+
+
+class WhatsAppNotification(NotificationStrategy):
+    """ Concrete implementation of a notification strategy"""
+    
+    def __init__(self, key: str, url: str):
+        """initiate all the required dependencies"""
+        self.key = key
+        self.url = url
+    
+    def notify(self, recipient: str, news: str):
+        print(f"{news} successfully notified to {recipient} via whatsapp")
