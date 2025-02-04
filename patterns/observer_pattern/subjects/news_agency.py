894a1a9ec1010b5b1f7ae803d1c2d02ab9f3af39
Satta Sundar Talukdar <bappu.ctg@gmail.com>
2025-02-04 17:40:51 +0600

    add: add the publish_news and notify_users method in the subject (news_agency)

--- patterns/observer_pattern/subjects/news_agency.py
+++ patterns/observer_pattern/subjects/news_agency.py
@@ -1,8 +1,10 @@
 from patterns.observer_pattern.interface.observer import Observer
 
+
 class NewsAgency:
     """ This will publish news to observer (Notification Manager) """
-    def __init__(self, observers: list[Observer]=None):
+    
+    def __init__(self, observers: list[Observer] = None):
         self._observers = observers if observers else []
     
     def add_observer(self, observer: Observer):
@@ -11,5 +13,16 @@ class NewsAgency:
     def remove_observer(self, observer: Observer):
         self._observers.remove(observer)
     
-    def notify_observer(self):
-        pass
+    def notify_users(self, recipients: list[dict], news: str):
+        """ Let's assume we have thousands of users (recipients) and we will notify them using via email, whatsapp, message and so on.
+            In our case here, we have only one observer (notification manager). In this dynamic approach, We can add more observers dynamically
+            without breaking or touching the core logic.
+        """
+        for observer in self._observers:
+            for recipient in recipients:
+                observer.update(recipient, news)
+    
+    def publish_news(self, recipients: list[dict], news: str):
+        """ Let's perform all the tasks related to publishing news first. Once news is published, we will call notify_users method to send the notifications  """
+        print("News published successfully")
+        self.notify_users(recipients, news)
